93ea67233b784d8f4812050e3af06c4893e8ac59
Megan Paffrath <meganpaffrath@gmail.com>
2022-12-31 13:19:31 -0500

    main is main duhh wee

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:meganpaffrath/guitar-hours.git master:gh-pages
+git push -f git@github.com:meganpaffrath/guitar-hours.git main:gh-pages
 cd -
