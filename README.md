698d4a13e106d6d8a203d0d3fb08bda62adbb08d
Megan Paffrath <meganpaffrath@gmail.com>
2022-12-31 13:59:23 -0500

    cleanup and plans

--- README.md
+++ README.md
@@ -23,3 +23,6 @@ yarn lint
 ### Customize configuration
 See [Configuration Reference](https://cli.vuejs.org/config/).
 # guitar-hours
+
+# Things I've Used
+- [Deploying Vue Apps to Github Pages](https://medium.com/swlh/deploy-vue-app-to-github-pages-2ada48d7397e)
--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <GuitarData msg="Oh, hi."/>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import GuitarData from './components/GuitarData.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    GuitarData
   }
 }
 </script>
